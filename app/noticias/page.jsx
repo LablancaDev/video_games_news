3231ed83416092caaaea2085ac550bb80d192394
LablancaDev <valenciano_david@hotmail.com>
2025-05-18 16:49:02 +0200

    update news

--- app/noticias/page.jsx
+++ app/noticias/page.jsx
@@ -186,7 +186,6 @@ export default function NoticiasPage() {
                   </div>
                 </Link>
               </SwiperSlide>
-              
             ))}
           </Swiper>
         </section>
